eac938597336d437c89b852516f1c1ad92d422e4
Daniel Hood <hood.danielc@gmail.com>
2015-07-19 10:32:40 -0700

    feature(build): adds some build scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 SRC=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
 cd $SRC
-$SRC/deps/ib/ib --src_root . --out_root $SRC/out main
+$SRC/deps/ib/ib --src_root . --out_root $SRC/out main --print_cfg --print_script --print_args
---
+++ scripts/start.sh
@@ -0,0 +1,3 @@
+SRC=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/..
+cd $SRC
+LD_LIBRARY_PATH=/usr/local/lib $SRC/out/debug/main
